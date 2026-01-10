ee3ab283962b8867f813aca1a9517d5630842650
wazidrazi <wazidrazi20@gmail.com>
2026-01-10 22:06:14 +0600

    React prac-1

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+    let products = ["Laptop", "Mouse", "Keyboard"];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>{product}</li>
+                ))}
+            </ul>
+
+            <FruitsName/>
+
+        </div>
+    );
 }
 
-export default App
+
+function FruitsName(){
+  let fruits = ["Apple", "Banana", "Mango"];
+
+    return (
+      <div>
+        <h2>Fruits Name</h2>
+        <ul>
+          {fruits.map((fruit,index) =>
+          <li key={index}> {fruit} </li>
+          )}
+        </ul>
+      </div>
+    );
+}
+
+export default App;
