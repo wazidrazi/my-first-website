5076854571ad4fdda8c39abb61d9ccac19fbcc55
wazidrazi <wazidrazi20@gmail.com>
2026-01-11 21:22:07 +0600

    react prac2

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -95,25 +95,52 @@
 // export default App;
 
 
+// function App(){
+//   return (
+//     < StudentCard name = "Wazid" age={25} marks={69} />
+//   );
+// }
+
+// function StudentCard({name, age, marks}){
+//   return(
+//     <div>
+//       <p>Name: {name} </p>
+//       <p>Age: {age} </p>
+//       < Result marks={marks} />
+//     </div>
+//   );
+// }
+
+// function Result({marks}){
+//   return(
+//     <p>Result: {marks >= 40 ? "Pass" : "Fail"} </p>
+//   );
+// }
+// export default App;
+
+
+
+
 function App(){
-  return (
-    < StudentCard name = "Wazid" age={25} marks={69} />
+  return(
+    <PlayerCard name="Wazid" score={82} />
   );
 }
 
-function StudentCard({name, age, marks}){
+function PlayerCard({name, score}){
   return(
     <div>
-      <p>Name: {name} </p>
-      <p>Age: {age} </p>
-      < Result marks={marks} />
+      <p>
+        {name} <Performance score={score} />
+      </p>
     </div>
   );
 }
 
-function Result({marks}){
+function Performance({score}){
   return(
-    <p>Result: {marks >= 40 ? "Pass" : "Fail"} </p>
+   <p> {score >= 50 ? "Qualified" : "Not Qualified"} </p>
   );
 }
+
 export default App;
