238d41cb3321b1eb50666d03092b0256df9d18e9
wazidrazi <wazidrazi20@gmail.com>
2026-01-11 20:55:17 +0600

    react result

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -35,21 +35,85 @@
 // export default App;
 
 
+// function App(){
+//   return(
+//     <div>
+//       <Greeting name = "Wazid" age ={25} />
+//     </div>
+//   );
+// }
+
+// function Greeting(props){
+//   return(
+//     <h2>
+//       Hello {props.name} <br/>
+//       Age: {props.age}
+//     </h2>
+//   );
+// }
+
+// export default App;
+
+
+
+// function App(){
+//   return(
+//     <div>
+//       <Greeting name = "Wazid" age ={25} />
+//     </div>
+//   );
+// }
+
+// function Greeting({name, age}){
+//   return(
+//     <h2>
+//       {name} - {age}
+//     </h2>
+//   );
+// }
+
+// export default App;
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Student student="Wazid" marks={87} />
+//     </div>
+//   );
+// }
+
+// function Student({ student, marks }) {
+//   return (
+//     <h2>
+//       Student: {student} <br />
+//       Marks: {marks}
+//     </h2>
+//   );
+// }
+
+// export default App;
+
+
 function App(){
+  return (
+    < StudentCard name = "Wazid" age={25} marks={69} />
+  );
+}
+
+function StudentCard({name, age, marks}){
   return(
     <div>
-      <Greeting name = "Wazid" age ={25} />
+      <p>Name: {name} </p>
+      <p>Age: {age} </p>
+      < Result marks={marks} />
     </div>
   );
 }
 
-function Greeting(props){
+function Result({marks}){
   return(
-    <h2>
-      Hello {props.name} <br/>
-      Age: {props.age}
-    </h2>
+    <p>Result: {marks >= 40 ? "Pass" : "Fail"} </p>
   );
 }
-
 export default App;
